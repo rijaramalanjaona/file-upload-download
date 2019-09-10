3aa2d1b15b795eb7d75e75651fa7dd3d36cd4e49
rijaramalanjaona <rijamamitiana.ramalanjaona@ingesup.com>
2019-09-10 17:32:05 +0200

    app module + component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ListUploadComponent } from './list-upload/list-upload.component';
+import { FormUploadComponent } from './form-upload/form-upload.component';
+import { DetailsUploadComponent } from './details-upload/details-upload.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UploadFileService } from './service/upload-file.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ListUploadComponent,
+		FormUploadComponent,
+		DetailsUploadComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule
+	],
+	providers: [UploadFileService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
